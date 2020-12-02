76398e4496a84fd0a7ded511d008374ad4e656f3
wookkl <wjddnr315@gmail.com>
2020-12-03 01:25:34 +0900

    tree

--- [1167]트리의 지름.py
+++ [1167]트리의 지름.py
@@ -15,10 +15,42 @@ class Node:
 class Tree:
     def __init__(self):
         self.root = None
+        self.diameter = 0
+        self.farthestNode = None
+
+    def make_tree(self, node1, node2, distance):
+        node1.childNode[node2] = distance
+
+    def dfs(self, startNode):
+        vstd = [False] * (n + 1)
+        stack = [[startNode, 0]]
+        diameter = 0
+        while stack:
+            for _ in range(len(stack)):
+                currNode, distance = stack[-1]
+                if vstd[currNode.data]:
+                    stack.pop()
+                    diameter -= distance
+                    continue
+                vstd[currNode.data] = True
+                diameter += distance
+                if self.diameter < diameter:
+                    self.diameter = diameter
+                    self.farthestNode = currNode
+                for childNode in currNode.childNode:
+                    if not vstd[childNode.data]:
+                        stack.append([childNode, currNode.childNode[childNode]])
 
 
 tree = Tree()
-
-for _ in range(int(input())):
+n = int(input())
+nodes = [Node(i) for i in range(n + 1)]
+for _ in range(n):
     inputs = list(map(int, input().split()))
-    print(inputs)
+    inputs.pop()
+    node = inputs.pop(0)
+    for i in range(0, len(inputs), 2):
+        tree.make_tree(nodes[node], nodes[inputs[i]], inputs[i + 1])
+tree.dfs(nodes[1])
+tree.dfs(tree.farthestNode)
+print(tree.diameter)
